7d13bbee5da33bf65beff1d3693c10d9c69d1b84
woof <wooftown@yandex.ru>
2020-02-06 14:28:54 +0300

    redundant Assert. in Tests

--- test/MyClass/Tests.java
+++ test/MyClass/Tests.java
@@ -1,21 +1,23 @@
 package MyClass;
 
-import org.junit.Assert;
+
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+
+import static org.junit.Assert.*;
 
 public class Tests {
 
     @Test
     public void addHuman(){
         final AddressBook book = new AddressBook();
-        Assert.assertTrue(book.addHuman("Овсянников","Науки",47,101));
-        Assert.assertFalse(book.addHuman("Овсянников","Науки",47,1110));
-        Assert.assertTrue(book.addHuman("Мазнин","Политехническая",17,2));
-        Assert.assertFalse(book.addHuman("Овсянников","Политехническая",17,2));
-        Assert.assertTrue(book.addHuman("Черныш","Политехническая",17,2));
+        assertTrue(book.addHuman("Овсянников","Науки",47,101));
+        assertFalse(book.addHuman("Овсянников","Науки",47,1110));
+        assertTrue(book.addHuman("Мазнин","Политехническая",17,2));
+        assertFalse(book.addHuman("Овсянников","Политехническая",17,2));
+        assertTrue(book.addHuman("Черныш","Политехническая",17,2));
     }
 
     @Test
@@ -23,9 +25,9 @@ public class Tests {
         final AddressBook book = new AddressBook();
         book.addHuman("Овсянников","Науки",47,101);
 
-        Assert.assertFalse(book.deleteHuman("Попов"));
-        Assert.assertTrue(book.deleteHuman("Овсянников"));
-        Assert.assertFalse(book.deleteHuman("Овсянников"));
+        assertFalse(book.deleteHuman("Попов"));
+        assertTrue(book.deleteHuman("Овсянников"));
+        assertFalse(book.deleteHuman("Овсянников"));
     }
 
     @Test
@@ -34,9 +36,9 @@ public class Tests {
         book.addHuman("Овсянников","Науки",47,101);
         book.addHuman("Мазнин","Политехническая",17,2);
 
-        Assert.assertEquals("Науки 47 101",book.findAddress("Овсянников"));
-        Assert.assertEquals("Политехническая 17 2",book.findAddress("Мазнин"));
-        Assert.assertNull(book.findAddress("Картелев"));
+        assertEquals("Науки 47 101",book.findAddress("Овсянников"));
+        assertEquals("Политехническая 17 2",book.findAddress("Мазнин"));
+        assertNull(book.findAddress("Картелев"));
     }
 
     @Test
@@ -45,9 +47,9 @@ public class Tests {
         book.addHuman("Овсянников","Науки",47,101);
         final String address = book.findAddress("Овсянников");
 
-        Assert.assertFalse(book.changeAddress("Овсянников","Науки",47,101));
-        Assert.assertTrue(book.changeAddress("Овсянников","Науки",45,101));
-        Assert.assertNotEquals(address,book.findAddress("Овсянников"));
+        assertFalse(book.changeAddress("Овсянников","Науки",47,101));
+        assertTrue(book.changeAddress("Овсянников","Науки",45,101));
+        assertNotEquals(address,book.findAddress("Овсянников"));
     }
 
     @Test
@@ -59,11 +61,11 @@ public class Tests {
         book.addHuman("Кришталь","Науки",47,52);
         book.addHuman("Железняк","Тихорецкий",34,85);
 
-        Assert.assertEquals(Arrays.asList("Овсянников", "Алтухов", "Кришталь"),book.findPersons("Науки"));
-        Assert.assertEquals(Collections.emptyList(),book.findPersons("Бутлерова"));
-        Assert.assertNotEquals(Arrays.asList("Овсянников", "Алтухов", "Кришталь"),book.findPersons("Науки",47));
-        Assert.assertEquals(Arrays.asList("Овсянников", "Кришталь"),book.findPersons("Науки",47));
-        Assert.assertEquals(Collections.emptyList(),book.findPersons("Тихорецкий",48));
-        Assert.assertEquals(book.findPersons("Тихорецкий"),book.findPersons("Тихорецкий",34));
+        assertEquals(Arrays.asList("Овсянников", "Алтухов", "Кришталь"),book.findPersons("Науки"));
+        assertEquals(Collections.emptyList(),book.findPersons("Бутлерова"));
+        assertNotEquals(Arrays.asList("Овсянников", "Алтухов", "Кришталь"),book.findPersons("Науки",47));
+        assertEquals(Arrays.asList("Овсянников", "Кришталь"),book.findPersons("Науки",47));
+        assertEquals(Collections.emptyList(),book.findPersons("Тихорецкий",48));
+        assertEquals(book.findPersons("Тихорецкий"),book.findPersons("Тихорецкий",34));
     }
 }
